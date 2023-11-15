a7e4b534114255c26bb90a969cc8b09fc8e3b9ac
critizero <critizero@gmail.com>
2023-11-15 09:13:54 +0800

    对接基线API

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ import threading
 import subprocess
 import time
 
+import uuid
+from producer import *
 
 class NDFuzzMonitor:
-    def __init__(self):
+    def __init__(self, message = None):
         with open('config.json', 'r') as conf_f:
             self.config = json.load(conf_f)
 
-        self.local_ip = '10.26.81.61'
+        self.local_ip = '10.26.81.7'
 
         self.logger = logging.getLogger('BaseLineMonitor')
         formatter = logging.Formatter('%(asctime)s - %(levelname)s: %(message)s')
@@ -25,10 +27,23 @@ class NDFuzzMonitor:
         self.logger.addHandler(console)
         self.logger.setLevel(logging.DEBUG)
 
-        self.time_limit = 300
         self.result_turn = 0
+
+        if message:
+            self.vendor = message["paras"]["vendor"]
+            self.protcol = message["paras"]["protocol"]
+            self.time_limit = message["paras"]["time_limit"]
+            self.time_gap = message["paras"]["time_gap"]
+        else:
+            # for local test
+            self.vendor = "srx"
+            self.protcol = "snmp"
+            self.time_limit = 300
+            self.time_gap = 0
+
         self.pre_result_idx = None
         self.res_pre = None
+        # self.res_new = None
         self.coverage = None
 
         self.start_image_flag = False
@@ -39,24 +54,30 @@ class NDFuzzMonitor:
         self.firmware_link = None
         self.fuzzer_link = None
 
+        self.message = message
+
     def error(self, error_msg):
         self.logger.error(error_msg)
         exit(0)
 
-    def start(self, vendor, protocol):
-        if vendor not in self.config or protocol not in self.config[vendor]:
+    def start(self):
+
+        if self.vendor not in self.config or self.protocol not in self.config[self.vendor]:
             self.error("No corresponding image file")
             return
 
-        config = self.config[vendor][protocol]
-        out_path = "{}/out_{}_{}_BY_BLM".format(config["fuzzer"], vendor, protocol)
+        config = self.config[self.vendor][self.protocol]
+        out_path = "{}/out_{}_{}_BY_BLM".format(config["fuzzer"], self.vendor, self.protocol)
         # out_path = "{}/out_srx_zsnmp_fb_1116/".format(config["fuzzer"])
         self.logger.info("[!] Output file path : {}".format(out_path))
 
         # 本地结果
-        self.res_pre = "log/{}_{}_pre.txt".format(vendor, protocol)
+        self.res_pre = "log/{}_{}_pre.txt".format(self.vendor, self.protocol)
+        # self.res_new = "log/{}_{}_new.txt".format(vendor, protocol)
         with open(self.res_pre, "a+") as res_pre:
             res_pre.truncate(0)
+        # with open(self.res_new, "a+") as res_new:
+        #     res_new.truncate(0)
         os.system("rm -f result/*")
 
         self.logger.debug('[+] Getting Running Port')
@@ -71,10 +92,10 @@ class NDFuzzMonitor:
         self.logger.info("[!] Image running at Port {}".format(port))
 
         self.logger.debug('[+] Starting Fuzzer Image')
-        t_image = threading.Thread(target=self.start_image, args=(config["image_start"],))
-        self.start_image_flag = False
-        self.end_image_flag = False
-        t_image.start()
+        # t_image = threading.Thread(target=self.start_image, args=(config["image_start"],))
+        # self.start_image_flag = False
+        # self.end_image_flag = False
+        # t_image.start()
 
         self.logger.debug('[+] Running firmware Image')
         t_firmware = threading.Thread(target=self.start_firmware, args=(config["firmware"], port))
@@ -94,7 +115,7 @@ class NDFuzzMonitor:
             time.sleep(1)
             exec_time += 1
 
-            if exec_time % 60 == 0:
+            if exec_time % self.time_gap == 0:
                 t_scan = threading.Thread(target=self.scan_result, args=(port, out_path))
                 t_scan.start()
 
@@ -142,12 +163,12 @@ class NDFuzzMonitor:
         return transport
 
     def start_firmware(self, path, port):
-        # 等待镜像启动完成
-        while True:
-            if self.start_image_flag:
-                self.logger.debug("[+] Waiting Image Start for 20s...")
-                time.sleep(20)
-                break
+        # # 等待镜像启动完成
+        # while True:
+        #     if self.start_image_flag:
+        #         self.logger.debug("[+] Waiting Image Start for 20s...")
+        #         time.sleep(20)
+        #         break
 
         self.firmware_link = self.start_link(port)
 
@@ -155,16 +176,24 @@ class NDFuzzMonitor:
         ssh = paramiko.SSHClient()
         ssh._transport = self.firmware_link
 
-        stdin, stdout, stderr = ssh.exec_command("cd {} && sudo ls".format(path), get_pty=True)
-        time.sleep(1)
-        stdin.write("mima1234\n")
+        # stdin, stdout, stderr = ssh.exec_command("cd {} &&ls".format(path), get_pty=True)
+        # time.sleep(1)
+        # stdin.write("mima1234\n")
         # print(stdout.read().decode())
 
-        self.logger.info("[!] Run Firmware...")
+        # self.logger.info("[!] Set Firmware network...")
+        # stdin, stdout, stderr = ssh.exec_command("cd {} && sudo ./network_setup.sh".format(path), get_pty=True)
+        # time.sleep(1)
+        # stdin.write("mima1234\n")
+        # print(stdout.read().decode())
+
         self.start_firmware_flag = True
-        ssh.exec_command("cd {} && sudo ./network_setup.sh && sudo ./start_qemu.sh".format(path))
+        self.logger.info("[!] Run Firmware...")
+        stdin, stdout, stderr = ssh.exec_command("cd {} && sudo ./network_setup.sh && sudo ./start_qemu.sh".format(path), get_pty=True)
         # stdin, stdout, stderr = ssh.exec_command("cd {} && sudo ./start_qemu.sh".format(path),
         #                                          get_pty=True)
+        time.sleep(1)
+        stdin.write("mima1234\n")
 
     def start_fuzzer(self, path, port, config, seed, out):
         # 等待固件线程启动
@@ -180,9 +209,7 @@ class NDFuzzMonitor:
         ssh = paramiko.SSHClient()
         ssh._transport = self.fuzzer_link
 
-        stdin, stdout, stderr = ssh.exec_command("sudo cat {}/Configs/{}.config".format(path, config), get_pty=True)
-        time.sleep(1)
-        stdin.write("mima1234\n")
+        stdin, stdout, stderr = ssh.exec_command("cat {}/Configs/{}.config".format(path, config))
 
         lines = stdout.read().decode().split('\n')
         for line in lines:
@@ -192,7 +219,9 @@ class NDFuzzMonitor:
 
         self.logger.info("[!] Run Fuzzer...")
         self.start_fuzzer_flag = True
-        ssh.exec_command("cd {} && sudo python start.py -c {} -i {} -o {} --overwrite".format(path, config, seed, out))
+        stdin, stdout, stderr = ssh.exec_command("cd {} && sudo python start.py -c {} -i {} -o {} --overwrite".format(path, config, seed, out), get_pty=True)
+        time.sleep(1)
+        stdin.write("mima1234\n")
 
     def scan_result(self, port, out):
         self.logger.debug("[+] Result Scan {}".format(self.result_turn))
@@ -229,12 +258,48 @@ class NDFuzzMonitor:
             for name in new_list:
                 sftp.get("{}/crashes/{}".format(out, name), "result/{}".format(name))
                 pre_res.write(name + "\n")
+        
+        # with open(self.res_new, "w") as res_new:
+        #     res_new.write(" ".join(new_list))
 
         stdin, stdout, stderr = ssh.exec_command("tail -n 2 {} | head -n 1".format(self.coverage))
         coverage = stdout.read().decode().strip()
         self.logger.info("[!] Coverage : {}".format(coverage))
 
+        producer_message = self.generate_producer_message(new_list)
+
+        producer = TaskQueue()
+        producer.send_task_result(producer_message)
+
+    def generate_producer_message(self, new_list):
+        successed, error, data = self.get_result_data(new_list)
+
+        message = self.message
+        message["msg_id"] = str(uuid.uuid4())
+        message["msg_type"] = 2
+        message["destination"] = self.message["source"]
+        message["successed"] = successed
+        message["error"] = error
+        message["data_uri"] = "null"
+        message["data"] = data
+        message["timestamp"] = int(time.time())
+        message["signature"] = "null"
+
+        return message
+
+    def get_result_data(self, new_list):
+        result_list = []
+        for case_name in new_list:
+            with open(case_name, "r") as case_f:
+                content = case_f.readlines()
+                result_list.append(' '.join(content).strip())
+        successed = True
+        error = "null"
+        data = "\n".join(result_list)
+        return successed, error, data
+        
+
 
 if __name__ == '__main__':
     m = NDFuzzMonitor()
-    m.start("srx", "snmp")
+    m.start()
